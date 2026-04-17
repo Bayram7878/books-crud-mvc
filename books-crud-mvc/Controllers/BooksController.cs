afb4d9665be7b616894d422a3e6eab4e7d5ea6a0
Bayram7878 <arazbayram74@gmail.com>
2026-04-17 18:11:12 +0300

    Proje dosyası ekle.

---
+++ books-crud-mvc/Controllers/BooksController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using books_crud_mvc.Models;
+using System;
+using System.Linq;
+
+namespace books_crud_mvc.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BooksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var books = _context.Books.ToList();
+            return View(books);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(Book newBook)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(newBook);
+            }
+
+            _context.Books.Add(newBook);
+            _context.SaveChanges();
+
+
+            TempData["Success"] = "Kitap başarıyla eklendi";
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var book = _context.Books.FirstOrDefault(x => x.Id == id);
+
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Book updatedBook)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(updatedBook);
+            }
+
+            var book = _context.Books.Find(updatedBook.Id);
+
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+
+            book.Title = updatedBook.Title;
+            book.Author = updatedBook.Author;
+            book.Price = updatedBook.Price;
+            book.Stock = updatedBook.Stock;
+
+            _context.SaveChanges();
+
+            TempData["Success"] = "Kitap başarıyla güncellendi";
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Remove(int id)
+        {
+            var book = _context.Books.Find(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+
+            TempData["Success"] = "Kitap silindi";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ books-crud-mvc/Views/Books/Update.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace books_crud_mvc.Views.Books
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
