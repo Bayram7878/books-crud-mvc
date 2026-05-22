b2218d5ca4568bc6c206e14ddd36f0ea79e8909e
Bayram7878 <arazbayram74@gmail.com>
2026-05-22 21:22:59 +0300

    13. Hafta

---
+++ books-crud-mvc/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace books_crud_mvc.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
